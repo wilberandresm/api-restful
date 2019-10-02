79526dacfae7f2492d7334569f4ed7e6ae86a21f
wilberandresm <wilberandresm@hotmail.com>
2019-10-02 12:28:00 -0500

    commit 2
    cración de endpoint con GET y parámetros

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const express=require('express')
 const bodyParser= require('body-parser')
 
 const app=express()
-const port= process.env.PORT || 3001
+const port= process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.get('/hola/:name',(req,res) =>{
+    res.send({message:`Hola ${req.params.name} !.`})
+})
 
 
 app.listen(port,() =>{
