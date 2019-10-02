5e6a2a3186463fbf93d92b75a8478bf057a40969
wilberandresm <wilberandresm@hotmail.com>
2019-10-02 01:38:13 -0500

    commit 1

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express=require('express')
+const bodyParser= require('body-parser')
+
+const app=express()
+const port= process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+
+app.listen(port,() =>{
+    console.log(`API REST corriendo en  http://localhost:${port}`)
+} )
