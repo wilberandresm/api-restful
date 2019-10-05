96178a6416f1d535b71a39af4ba2538bbd1f31d1
wilberandresm <wilberandresm@hotmail.com>
2019-10-05 11:42:33 -0500

    commit 7
    refactorizando configuraciones

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 const mongoose=require('mongoose')
 const app=require('./app')
-const port= process.env.PORT || 3000
+const config= require('./config')
 
-mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false},(err,res) =>{
+
+mongoose.connect(config.db,{ useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false},(err,res) =>{
     if(err){
         return console.log(`error al conectar a la base de datos: ${err}`)
     }
     console.log('Conexión a la base de datos establecida.')
 
-    app.listen(port,() =>{
-        console.log(`API REST corriendo en  http://localhost:${port}`)
+    app.listen(config.port,() =>{
+        console.log(`API REST corriendo en  http://localhost:${config.port}`)
     } )
 })
